f78d45752855041cfb86016a56690e820eab1326
TodoLodo2089 <me@todolodo.xyz>
2022-07-14 22:23:30 +0530

    3

--- setup.py
+++ setup.py
@@ -6,18 +6,15 @@ with open("README.md", "r") as readme_file:
 requirements = ["requests", "uuid", "urllib3", "enum34"]
 
 setup(
-    name="cod api",
+    name="cod_api",
     version="0.0.1",
     author="Todo Lodo",
     author_email="me@todolodo.xyz",
-    description="A package to convert your Jupyter Notebook",
+    description="Call Of Duty API",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/TodoLodo2089/cod-python-api.git",
     packages=find_packages(),
     install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
+    classifiers=[],
 )

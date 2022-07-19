0486aa023933e8b6365048b0d539093e3b5fa2b6
TodoLodo2089 <me@todolodo.xyz>
2022-07-19 07:57:20 +0530

    Update, date - Tue 07/19/2022, time -  7:57:20.52

--- conf.py
+++
@@ -1,6 +0,0 @@
-extensions = ['sphinx_tabs.tabs', 'sphinxcontrib.fulltoc']
-sphinx_tabs_valid_builders = ['linkcheck']
-sphinx_tabs_disable_tab_closing = True
-def setup(app):
-   app.add_lexer('alias', MyCustomLexer())
-sphinx_tabs_disable_css_loading = True

8a85ce104f3e73a14410bc7cdae9e23b8fc59bff
Todo Lodo <me@todolodo.xyz>
2022-10-27 00:58:04 +0530

    Update, date - Thu 10/27/2022, time -  0:58:03.90
    Co-authored-by: Engineer15 <engineergamer15@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-requirements = ["asyncio", "datetime", "requests", "uuid", "urllib3", "enum34", "json", "sys"]
+requirements = ["asyncio", "datetime", "requests", "uuid", "urllib3", "enum34"]
 
 setup(
     name="cod_api",
